d1b8593ca0f4f0b80304e55f5daaf9be8c86c156
Horváth Zsolt <zsolt.horvath@wst.hu>
2020-09-30 11:00:57 +0200

    fixed script

--- import.fish
+++ import.fish
@@ -1,7 +1,12 @@
-for xml in $DIR_XML*.xml
+#!/usr/bin/env fish
+
+cd ~/Documents/import
+source source.fish
+
+for xml in $DIR_INCOMING/*.xml
 	set filename (basename $xml)
 	set rootname (echo $filename | sed 's/\.[^.]*$//')
 	saxon -s:$xml -xsl:transform/nicefood.xsl -o:$DIR_TEMP/$rootname.html
-	saxon -s:$xml -xsl:transform/ngm.xsl -o:$DIR_TEMP/$rootname.html
+	saxon -s:$xml -xsl:transform/ngm.xsl -o:$DIR_NGM/$rootname.html
 	wkhtmltopdf -B 10mm -T 5mm --header-spacing 5 --footer-center "[page] oldal" --footer-font-size 8 $DIR_TEMP/$rootname.html $DIR_PDF/$rootname.pdf
  end
